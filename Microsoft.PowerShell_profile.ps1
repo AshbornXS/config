a687bd277fbe1d20cc009f9849ece8424a8f07eb
Pedro Azevedo <75546030+AshbornXS@users.noreply.github.com>
2025-01-07 13:17:36 -0300

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/.mytheme.omp.json" | Invoke-Expression
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
